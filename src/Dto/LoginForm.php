1e19b91c5fd1097669d19e77101b60bb480ab125
AREZA13 <arezasoft@icloud.com>
2024-01-12 18:29:23 +0300

    Login & Register forms DTOs added

---
+++ src/Dto/LoginForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Dto;
+
+
+use Exception;
+
+readonly class  LoginForm
+{
+    public function __construct(
+        public string $login,
+        public string $password
+    )
+    {
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function fromUserRequest(): self
+    {
+        $requestAsArray = json_decode(file_get_contents('php://input'), true);
+        if (empty($requestAsArray['login'])) {
+            throw new Exception('Login is empty!');
+        }
+        if (empty($requestAsArray['password'])) {
+            throw new Exception('Password is empty!');
+        }
+
+        return new self($requestAsArray['login'], $requestAsArray['password']);
+    }
+}
